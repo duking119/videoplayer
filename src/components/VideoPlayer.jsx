b7240127afccd5e5ffe96fa352ca7da50222ee60
duking119 <huoyanfeng7@gmail.com>
2025-11-13 19:36:14 +0800

    添加播放视窗缩放功能

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -8,21 +8,35 @@ import './CustomButton';
 const VideoPlayer = ({ options, onReady }) => {
    const videoRef = React.useRef(null);
    const playerRef = React.useRef(null);
+   const mergedOptions = React.useMemo(
+       () => ({
+           fluid: true,
+           aspectRatio: '16:9',
+           ...options,
+       }),
+       [options],
+   );
    React.useEffect(() => {
        if (!playerRef.current) {
            const videoElement = document.createElement('video-js');
            videoElement.classList.add('vjs-big-play-centered');
            videoRef.current.appendChild(videoElement);
 
-           const player = (playerRef.current = videojs(videoElement, options, () => {
+           const player = (playerRef.current = videojs(videoElement, mergedOptions, () => {
                if (onReady) onReady(player);
            }));
        } else {
            const player = playerRef.current;
-           player.autoplay(options.autoplay);
-           player.src(options.sources);
+           player.autoplay(mergedOptions.autoplay);
+           if (mergedOptions.sources) {
+               player.src(mergedOptions.sources);
+           }
+           if (mergedOptions.aspectRatio) {
+               player.aspectRatio(mergedOptions.aspectRatio);
+           }
+           player.fluid(Boolean(mergedOptions.fluid));
        }
-   }, [options]);
+   }, [mergedOptions, onReady]);
    React.useEffect(() => {
        return () => {
            if (playerRef.current) {
@@ -31,6 +45,10 @@ const VideoPlayer = ({ options, onReady }) => {
            }
        };
    }, []);
-   return <div data-vjs-player><div ref={videoRef} /></div>;
+   return (
+       <div className="video-player-wrapper" data-vjs-player>
+           <div ref={videoRef} />
+       </div>
+   );
 };
 export default VideoPlayer;
